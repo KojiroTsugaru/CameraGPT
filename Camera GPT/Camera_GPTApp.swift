902aec4cbb37cfd23e66f0eebf6de77008d55d39
Kojiro Tsugaru <kojirotsugaru@gmail.com>
2023-01-06 16:29:50 -0800

    Initial Commit

---
+++ Camera GPT/Camera_GPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_GPTApp.swift
+//  Camera GPT
+//
+//  Created by KJ on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Camera_GPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Camera GPT/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Camera GPT
+//
+//  Created by KJ on 1/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
